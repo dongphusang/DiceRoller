cf940880623b9c40df5ba63e56905fb2645ebe1a
dongphusang <marcodsang@gmail.com>
2022-04-19 21:55:24 -0400

    Roll Logic Implementation:
    - Enabled 'roll once' button to display one result
    - Enabled 'roll twice' button to display two results
    - Enabled users to choose from several available dice
    - Force users to choose a die before rolls by making the roll buttons do
    nothing but throwing Toast saying 'please choose a die'

---
+++ app/src/main/java/com/github/diceroller/DieData.java
@@ -0,0 +1,38 @@
+package com.github.diceroller;
+
+import java.util.Random;
+
+public class DieData {
+
+    // declaring atts
+    private int numberOfSides;
+    private int firstResult;
+    private int secondResult;
+    private Random rand = new Random();
+
+    public DieData (){
+        numberOfSides = 0;
+        firstResult = 0;
+        secondResult = 0;
+    }
+
+    public void setNumberOfSides(int numbSide){
+        numberOfSides = numbSide;
+    }
+
+    public int getNumberOfSides(){
+        return numberOfSides;
+    }
+
+    public int rollDieOnce(){
+        // since the results will be from 0, add one to shift the value by 1 unit
+        return rand.nextInt(numberOfSides)+1;
+    }
+
+    public int[] rollDieTwice(){
+        firstResult = rand.nextInt(numberOfSides)+1;
+        secondResult = rand.nextInt(numberOfSides)+1;
+
+        return new int[]{firstResult,secondResult};
+    }
+}
--- app/src/main/java/com/github/diceroller/MainActivity.java
+++ app/src/main/java/com/github/diceroller/MainActivity.java
@@ -3,12 +3,111 @@ package com.github.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    // declaring components
+    TextView firstResult;
+    TextView secondResult;
+    Button rollOnceButton;
+    Button rollTwiceButton;
+    Button fourSidedDie;
+    Button sixSidedDie;
+    Button eightSidedDie;
+    Button tenSidedDie;
+    Button twelveSidedDie;
+    Button twentySidedDie;
+    DieData dieManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // init components
+        firstResult = findViewById(R.id.first_result);
+        secondResult = findViewById(R.id.second_result);
+        rollOnceButton = findViewById(R.id.roll_once);
+        rollTwiceButton = findViewById(R.id.roll_twice);
+        fourSidedDie = findViewById(R.id.four_sided_die);
+        sixSidedDie = findViewById(R.id.six_sided_die);
+        eightSidedDie = findViewById(R.id.eight_sided_die);
+        tenSidedDie = findViewById(R.id.ten_sided_die);
+        twelveSidedDie = findViewById(R.id.twelve_sided_die);
+        twentySidedDie = findViewById(R.id.twenty_sided_die);
+        dieManager = new DieData();
+
+        // assign listeners
+        rollOnceButton.setOnClickListener(this);
+        rollTwiceButton.setOnClickListener(this);
+        fourSidedDie.setOnClickListener(this);
+        sixSidedDie.setOnClickListener(this);
+        eightSidedDie.setOnClickListener(this);
+        tenSidedDie.setOnClickListener(this);
+        twelveSidedDie.setOnClickListener(this);
+        twentySidedDie.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.roll_once:
+                if (dieManager.getNumberOfSides() == 0)
+                    Toast.makeText(this,"Choose one damn die",Toast.LENGTH_SHORT).show();
+                else {
+                    firstResult.setText(""+dieManager.rollDieOnce());
+                    secondResult.setText("0");
+                }
+                break;
+            case R.id.roll_twice:
+                if (dieManager.getNumberOfSides() == 0)
+                    Toast.makeText(this,"Choose one damn die",Toast.LENGTH_SHORT).show();
+                else {
+                    int[]results = dieManager.rollDieTwice();
+                    firstResult.setText(""+results[0]);
+                    secondResult.setText(""+results[1]);
+                }
+                break;
+            case R.id.four_sided_die:
+                dieManager.setNumberOfSides(4);
+                firstResult.setText("0");
+                secondResult.setText("0");
+                Toast.makeText(this, "Four sided die selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.six_sided_die:
+                dieManager.setNumberOfSides(6);
+                firstResult.setText("0");
+                secondResult.setText("0");
+                Toast.makeText(this, "Six sided die selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.eight_sided_die:
+                dieManager.setNumberOfSides(8);
+                firstResult.setText("0");
+                secondResult.setText("0");
+                Toast.makeText(this, "Eight sided side selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ten_sided_die:
+                dieManager.setNumberOfSides(10);
+                firstResult.setText("0");
+                secondResult.setText("0");
+                Toast.makeText(this, "Ten sided side selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.twelve_sided_die:
+                dieManager.setNumberOfSides(12);
+                firstResult.setText("0");
+                secondResult.setText("0");
+                Toast.makeText(this, "Twelve sided die selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.twenty_sided_die:
+                dieManager.setNumberOfSides(20);
+                firstResult.setText("0");
+                secondResult.setText("0");
+                Toast.makeText(this, "Twenty sided die selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
